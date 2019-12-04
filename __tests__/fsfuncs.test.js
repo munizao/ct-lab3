767a58be2031f5be552aacf91a6ef079be3fe3a4
Alexandre Muniz <aom@puzzlezapper.com>
2019-12-04 15:22:45 -0800

    add test for readJSON

--- __tests__/fsfuncs.test.js
+++ __tests__/fsfuncs.test.js
@@ -1,12 +1,15 @@
 const fs = require('fs').promises;
 
 const {
-  mkdirp
+  mkdirp,
+  writeJSON,
+  readJSON
 } = require('../lib/fsfuncs.js');
 
 jest.mock('fs', () => ({
   promises: {
-    mkdir: jest.fn(() => Promise.resolve())
+    mkdir: jest.fn(() => Promise.resolve()),
+    writeFile: jest.fn(() => Promise.resolve())
   }
 }));
 
@@ -16,13 +19,19 @@ describe ('fsfuncs module', () => {
       .then(expect(fs.mkdir).toHaveBeenCalledWith('some/nested/dirs', { recursive: true }));
   });
 
-  it('writes JSON to files', () => {
+  it('writes an obect as JSON to a file', () => {
     const obj = {
-      name: 'frodo'
+      name: 'frodo',
       age: 33
     };
+    const json = JSON.stringify(obj);
     return writeJSON('out.json', obj)
-      .then(expect(fs.writeFile).toHaveBeenCalledWith('out.json', 'FIGURETHISOUT'))
+      .then(expect(fs.writeFile).toHaveBeenCalledWith('out.json', json));
+  });
+
+  it('reads a file with a JSON string to an object', () => {
+    const obj = readJSON('obj.json')
+    .then(expect(fs.readFile).toHaveBeenCalledWith('obj.json'));
   });
 });
 
